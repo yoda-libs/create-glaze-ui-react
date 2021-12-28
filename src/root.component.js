a2e30cc1dab75fb6b72036572966cf0fca6f4c7d
Juliano Teixeira <jzucchetto@gmail.com>
2021-12-28 13:22:34 -0800

    fixed typo

--- src/root.component.js
+++ src/root.component.js
@@ -15,7 +15,7 @@ export default function Root(props) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Navbarr</Navbar.Brand>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
