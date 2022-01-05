6384270cbdf47b212cf8f460c15fe19bbaab33d0
Juliano Teixeira <jzucchetto@gmail.com>
2022-01-05 11:31:20 -0800

    improved root component semantic

--- src/root.component.js
+++ src/root.component.js
@@ -3,24 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { useEffect } from 'react';
 
 export default function Root(props) {
-  // subscribe to messages from glaze framework
-  var sub = props.glaze.subscribe(msg => {
-    console.log(`[${props.name} recv]`, msg);
+  const { glaze, name } = props;
+  useEffect(() => {
+    // subscribe to messages from glaze framework
+    var sub = glaze.subscribe(msg => {
+      console.log(`[${name} recv]`, msg);
+    });
+    console.log(`[${name}] subscribed to glaze`);
 
-    // unsubscribe when not needed anymore
-    sub.unsubscribe();
+    return function unsubscribe() {
+        // unsubscribe when not needed anymore
+        sub.unsubscribe();
+        console.log(`[${name}] unsubscribed from glaze`);
+    }
+
   });
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="sm">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="/">Navbar</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
