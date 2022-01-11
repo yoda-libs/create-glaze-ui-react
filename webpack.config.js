69357f2abf974cca5772cce730775ce3253004af
Juliano Teixeira <jzucchetto@gmail.com>
2022-01-11 14:36:36 -0800

    fixed hmr

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,45 @@ const path = require('path');
 const SystemJSPublicPathWebpackPlugin = require("systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin");
 const name = require('./package.json').name;
 
-module.exports = {
-  mode: "development",
-  context: path.resolve(__dirname, 'src'),
-  entry: ['react-hot-loader/patch', `./${name}.js`],
-  plugins: [
-    new SystemJSPublicPathWebpackPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  devtool: 'source-map',
-  devServer: {
-    hot: true,
-    historyApiFallback: true,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    client: { webSocketURL: { hostname: 'localhost' } },
-    allowedHosts: 'all'
-  },
-  externals: [ 'react', 'react-dom' ],
-  output: {
-    libraryTarget: 'system', 
-    clean: true,
-    filename: `${name}.js` // name of the generated bundle
-  }
+module.exports = (env, options) => {
+  const isDev = options.mode !== 'production';
+
+  return {
+    mode: isDev ? 'development' : 'production',
+    context: path.resolve(__dirname, 'src'),
+    entry: [`./${name}.js`],
+    plugins: [
+      new SystemJSPublicPathWebpackPlugin(),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    devtool: 'source-map',
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      client: { webSocketURL: { hostname: 'localhost' } },
+      allowedHosts: 'all'
+    },
+    externals: [ 'react', 'react-dom' ],
+    output: {
+      libraryTarget: 'system', 
+      clean: true,
+      filename: `${name}.js`, // name of the generated bundle
+      uniqueName: `${name}`,
+      devtoolNamespace: `${name}`,
+      publicPath: "",
+    }
+  };
 };
-
-
-
-
-
